6765bfa715fd6c5a92232c1f65662e7bf95f42ac
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2023-06-07 22:47:09 +0900

    delete random class

--- source.cpp
+++ source.cpp
@@ -2,33 +2,17 @@
 #include <iostream>
 #include <bitset>
 #include <deque>
-class Random {
-private:
-	~Random() = default;
-	std::mt19937 engine;
-	Random() :engine(std::random_device()()) {}
-public:
-	Random(const Random&) = delete;
-	Random(const Random&&) = delete;
-	Random& operator=(const Random&) = delete;
-	Random& operator=(const Random&&) = delete;
-	static auto& getInstance() {
-		static Random instance;
-		return instance;
-	}
-	auto get(const int min, const int max) {
-		return std::uniform_int_distribution<>(min, max)(engine);
-	}
-};
+std::default_random_engine engine((std::random_device())());
 auto generateRow(std::deque<int8_t > &lastrows) {
-	const auto lastlowpadded = (((Random::getInstance().get(0, 1) << 8) | lastrows.back()) << 1) | Random::getInstance().get(0, 1);
+	static std::uniform_int_distribution<> getRandomBit(0, 1);
+	const auto lastlowpadded = (((getRandomBit(engine) << 8) | lastrows.back()) << 1) | getRandomBit(engine);
 	lastrows.emplace_back(0);
 	for (auto i = 7, lasttwo = 0b10; 0 <= i; --i) {
 		bool newBit;
 		const std::bitset<5> key = (lasttwo << 3) | ((lastlowpadded >> i) & 0b111) ;
 		if (!key[2] && (!(key[1] || key[3])||!(key[4]) && (key[3])||!(key[0]) && (key[1])))newBit = 1;
 		else if ((key[2] && ((key >> 1) != 0b0011) && (key << 1) != 0b11000) || key == 0b11001)newBit=0;
-		else newBit = Random::getInstance().get(0,1);
+		else newBit = getRandomBit(engine);
 		lastrows.back() = (lastrows.back() << 1) | newBit;
 		lasttwo = ((lasttwo << 1) | newBit) & 0b11;
 	}
@@ -58,7 +42,7 @@ int main() {
 		generateRow(a);
 		auto bit = std::bitset<8>(a.back());
 		for (auto i = 0; i<bit.size();++i) {
-			std::cout <<(bit[i]?"â– ":"â–¡");
+			std::cout <<(bit[i]?"¡":" ");
 		}
 		std::cout << std::endl;
 	}
